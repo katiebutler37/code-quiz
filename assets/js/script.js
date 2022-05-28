ca38d2abca426dd6af3e75856e3537d739300e28
Katie <katiebutler37@gmail.com>
2022-05-28 09:48:58 -0400

    next question function, showquestion started

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,47 @@
+//get elements from HTMl index to target
 var startButtonEl = document.querySelector("#start-btn");
 var quizIntroEl = document.querySelector(".quiz-intro");
 var questionContainerEl = document.querySelector("#question-container");
+var randomizedQuestions;
+var questionIndex;
+var questionEl = document.querySelector("#question");
+var answersEl = document.querySelector("#answers")
 
-startButtonEl.addEventListener("click", startGame);
+//make an array to hold each question and answer together
+var questions = [
+    {
+        question: "How are you?",
+        //make an inner array to hold the 4 possible answers
+        answers: [
+            { answer: "", correct: true },
+            { answer: "", correct: false },
+            { answer: "", correct: false },
+            { answer: "", correct: false }
+        ]
+    }
+];
 
-function startGame () {
+//defined function to start game, that will lead to first question on click
+var startGame = function() {
     quizIntroEl.classList.add("hide");
+    randomizedQuestions = questions.sort(() => Math.random() - .5);
+    questionIndex = 0;
     questionContainerEl.classList.remove("hide");
+    nextQuestion();
+};
+
+var showQuestion = function(question) {
+    question = JSON.stringify(question);
+    questionEl.innerHTML = "<h2 id='question'>" + questions.question + "</h2>";
+};
+
+var nextQuestion = function() {
+    showQuestion(randomizedQuestions[questionIndex]);
 }
 
-function nextQuestion() {
+var selectAnswer = function() {
 
 }
 
-function selectAnswer() {
-
-}
+//call startGame function on click
+startButtonEl.addEventListener("click", startGame);
