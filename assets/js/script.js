f1d01c8615bf9d904194daa1ca9ea73db2fc9d58
Katie <katiebutler37@gmail.com>
2022-05-29 08:08:21 -0400

    debugging for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,7 @@
 var startButtonEl = document.querySelector("#start-btn");
 var quizIntroEl = document.querySelector(".quiz-intro");
 var questionContainerEl = document.querySelector("#question-container");
-var randomizedQuestions;
 var questionIndex;
-var questionEl = document.querySelector("#question");
-var answersEl = document.querySelector("#answers")
 
 //make an array to hold each question and answer together
 var questions = [
@@ -13,26 +10,33 @@ var questions = [
         question: "How are you?",
         //make an inner array to hold the 4 possible answers
         answers: [
-            { answer: "", correct: true },
-            { answer: "", correct: false },
-            { answer: "", correct: false },
-            { answer: "", correct: false }
+            { answer: "1", correct: true },
+            { answer: "2", correct: false },
+            { answer: "3", correct: false },
+            { answer: "4", correct: false }
         ]
     }
 ];
 
+for (var i = 0; i < questions.length; i++) {
+     var randomizedQuestions = questions.sort(() => Math.random() - .5);
+}
+console.log(questions[0].question);
+
+var questionEl = document.querySelector("#question");
+var answersEl = document.querySelector("#answers");
+
 //defined function to start game, that will lead to first question on click
 var startGame = function() {
     quizIntroEl.classList.add("hide");
-    randomizedQuestions = questions.sort(() => Math.random() - .5);
     questionIndex = 0;
     questionContainerEl.classList.remove("hide");
     nextQuestion();
 };
 
-var showQuestion = function(question) {
-    question = JSON.stringify(question);
-    questionEl.innerHTML = "<h2 id='question'>" + questions.question + "</h2>";
+//the questionObj parameter is the result of (randomizedQuestions[questionIndex]) ... the single object question (question/answers pair) that is provided to the showQuestion function from the nextQuestion function
+var showQuestion = function(questionObj) {
+    questionEl.innerHTML = "<h2 id='question'>" + questionObj.question + "</h2>";
 };
 
 var nextQuestion = function() {
