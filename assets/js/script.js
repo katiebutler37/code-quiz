e6697acb557eaa08bf57e05042b8d54c2edabfa3
Katie <katiebutler37@gmail.com>
2022-05-29 10:57:10 -0400

    answer text content printing on page:)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,29 +3,33 @@ var startButtonEl = document.querySelector("#start-btn");
 var quizIntroEl = document.querySelector(".quiz-intro");
 var questionContainerEl = document.querySelector("#question-container");
 var currentQuestionIndex;
-var randomizedQuestions;
-var randomizedAnswers;
+    currentQuestionIndex++;
+var randomizedQuestionSet;
+//var randomizedAnswers;
+var answerButtonEl = document.querySelector(".answer-btn");
 
 //make an array to hold each question and answer together
 var questions = [
     {
         question: "How are you?",
         //make an inner array to hold the 4 possible answers
-        answers: [
-            { answer: "1", correct: true },
-            { answer: "2", correct: false },
-            { answer: "3", correct: false },
-            { answer: "4", correct: false }
-        ]
+        answers: {
+            answer1: "1",
+            answer2: "2", 
+            answer3: "3",
+            answer4: "4",
+            correct: "1"
+        }
     },
     {
         question: "what's up?",
-        answers: [
-            { answer: "5", correct: true },
-            { answer: "6", correct: false },
-            { answer: "7", correct: false },
-            { answer: "8", correct: false }
-        ]
+        answers: {
+            answer1: "1",
+            answer2: "2", 
+            answer3: "3",
+            answer4: "4",
+            correct: "1"
+        }
     }
 ];
 
@@ -35,29 +39,34 @@ var answersEl = document.querySelector("#answers");
 //defined function to start game, that will lead to first question on click
 var startGame = function() {
     quizIntroEl.classList.add("hide");
-    randomizedQuestions = questions.sort(() => Math.random() - .5);
+    randomizedQuestionSet = questions.sort(() => Math.random() - .5);
     currentQuestionIndex = 0;
+    console.log(randomizedQuestionSet);
     currentQuestionIndex++;
     questionContainerEl.classList.remove("hide");
     nextQuestion();
 };
 
-//the questionObj parameter is the result of (randomizedQuestions[questionIndex]) ... the single object question (question/answers pair) that is provided to the showQuestion function from the nextQuestion function
+//the questionObj parameter is the result of (randomizedQuestionSet[questionIndex]) ... the single object question (question/answers pair) that is provided to the showQuestion function from the nextQuestion function
 var showQuestion = function(questionObj) {
     questionEl.innerHTML = "<h2 id='question'>" + questionObj.question + "</h2>";
+    answersEl.innerHTML = "<button class='answer-btn btn'>" + questionObj.answers.answer1 + "</button> <button class='answer-btn btn'>" + questionObj.answers.answer2 + "</button> <button class='answer-btn btn'>" + questionObj.answers.answer3 + "</button> <button class='answer-btn btn'>" + questionObj.answers.answer4 + "</button>";
 };
 
 var nextQuestion = function() {
-    showQuestion(randomizedQuestions[currentQuestionIndex]);
-    for (var i=0; i<questions[currentQuestionIndex].answers.length; i++) {
-        randomizedAnswers = questions[currentQuestionIndex].answers[Math.floor(Math.random() * answers.length)];
-        console.log(answer);
-    }
+    showQuestion(randomizedQuestionSet[currentQuestionIndex]);
+    /*for (var i=0; i<questions[currentQuestionIndex].answers.length; i++) {
+        randomizedAnswers = questions[currentQuestionIndex].answers[Math.floor(Math.random() * questions[currentQuestionIndex].answers.length)];
+        console.log(randomizedAnswers);
+    }*/
 };
 
 var selectAnswer = function() {
-
+    if 
+nextQuestion();
 }
 
 //call startGame function on click
 startButtonEl.addEventListener("click", startGame);
+
+answerButtonEl.addEventListener("click", selectAnswer)
