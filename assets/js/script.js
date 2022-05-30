ef905bd7e5e8f879578d86fea63d3eab45bc42c9
Katie <katiebutler37@gmail.com>
2022-05-30 15:14:19 -0400

    new bug after globalizing variables, attempting to fix with for loop instead of randomized

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,12 @@ var startButtonEl = document.querySelector("#start-btn");
 var quizIntroEl = document.querySelector(".quiz-intro");
 var questionContainerEl = document.querySelector("#question-container");
 var answerButtonEl;
-var currentQuestionIndex;
-var randomizedQuestionSet;
 var answerButtonEl;
 var viewHighScoresEl = document.querySelector("#high-scores");
 var feedbackEl = document.querySelector(".feedback");
 var cardContentEl = document.querySelector(".card-content");
 var countdownEl = document.querySelector("#countdown");
+
 
 //make an array to hold each question and answer together
 var questions = [
@@ -63,8 +62,14 @@ var questions = [
     }
 ];
 
+var randomizedQuestionSet = questions.sort(() => Math.random() - .5);
+var currentQuestionIndex = 0;
+var questionObj = randomizedQuestionSet[currentQuestionIndex];
+
+
 var questionEl = document.querySelector("#question");
 var answersEl = document.querySelector("#answers");
+var selectedAnswer;
 
 var clearAnswers = function() {
     answersEl.innerHTML = "";
@@ -72,54 +77,28 @@ var clearAnswers = function() {
 
 //defined function to start game, that will lead to first question on click
 var startGame = function() {
-    quizIntroEl.classList.add("hide");
-    randomizedQuestionSet = questions.sort(() => Math.random() - .5);
+    startCountdown();
     currentQuestionIndex = 0;
+    quizIntroEl.classList.add("hide");
     questionContainerEl.classList.remove("hide");
     nextQuestion();
 };
-
-// var finalQuestion = function(event) {
-//     var selectedAnswer = event.target;
-//     var questionObj = randomizedQuestionSet[currentQuestionIndex];
-//         if (selectedAnswer.value == questionObj.correct) {
-//         console.log("correct"); 
-//         selectedAnswer.style.backgroundColor="green";
-//         feedbackEl.textContent = "";
-//         feedbackEl = document.createElement("h3");
-//         feedbackEl.className = "feedback correct";
-//         feedbackEl.textContent = "You got it!"; 
-//         cardContentEl.appendChild(feedbackEl);
-//         setTimeout(endQuiz, 500);
-//     }
-//     else {
-//         console.log("wrong"); 
-//         selectedAnswer.style.backgroundColor="red";
-//         feedbackEl.textContent = "";
-//         feedbackEl = document.createElement("h3");
-//         feedbackEl.className = "feedback";
-//         feedbackEl.textContent = "Whoops! Not quite!"; 
-//         cardContentEl.appendChild(feedbackEl);
-//         setTimeout(endQuiz, 500);
-//     };
-// };
 
 //the questionObj parameter is the result of (randomizedQuestionSet[questionIndex]) ... the single object question (question/answers pair) that is provided to the nextQuestion function
 var nextQuestion = function() {
     console.log(currentQuestionIndex);
     console.log(randomizedQuestionSet.length);
-    var questionObj = randomizedQuestionSet[currentQuestionIndex];
     questionEl.textContent = questionObj.question;
     console.log(questionObj);
     clearAnswers();
     for (var i=0; i < questionObj.answers.length; i++) {
-        //var randomizedAnswers = questionsObj.answers[Math.floor(Math.random() * questionObj.answers.length)];
         answerButtonEl = document.createElement("button");
         answerButtonEl.className = "answer-btn btn";
         answerButtonEl.setAttribute("value", questionObj.answers[i]);
         answerButtonEl.textContent = questionObj.answers[i]; 
         answerButtonEl.addEventListener("click", selectAnswer);
         answersEl.appendChild(answerButtonEl);
+        console.log(answerButtonEl);
     }
         currentQuestionIndex++;
     
@@ -130,22 +109,35 @@ var endQuiz = function() {
     questionContainerEl.classList.add("hide");
 }
 
+var correctFeedback = function() {
+    console.log("correct"); 
+    selectedAnswer.style.backgroundColor="green";
+    feedbackEl.textContent = "";
+    feedbackEl = document.createElement("h3");
+    feedbackEl.className = "feedback correct";
+    feedbackEl.textContent = "You got it!"; 
+    cardContentEl.appendChild(feedbackEl);
+}
+
+var wrongFeedback = function() {
+    console.log("wrong"); 
+    selectedAnswer.style.backgroundColor="red";
+    feedbackEl.textContent = "";
+    feedbackEl = document.createElement("h3");
+    feedbackEl.className = "feedback";
+    feedbackEl.textContent = "Whoops! Not quite!"; 
+    cardContentEl.appendChild(feedbackEl);
+}
+
 var selectAnswer = function(event) {
-    var selectedAnswer = event.target;
-    //eliminate repetition later if possible
-    var questionObj = randomizedQuestionSet[currentQuestionIndex];
+    selectedAnswer = event.target;
     console.log (currentQuestionIndex);
         if (randomizedQuestionSet.length < currentQuestionIndex +1 && selectedAnswer.value == randomizedQuestionSet[currentQuestionIndex -1].correct) {
-            console.log("correct"); 
-            selectedAnswer.style.backgroundColor="green";
-            feedbackEl.textContent = "";
-            feedbackEl = document.createElement("h3");
-            feedbackEl.className = "feedback correct";
-            feedbackEl.textContent = "You got it!"; 
-            cardContentEl.appendChild(feedbackEl);
+            correctFeedback();
             setTimeout(endQuiz, 500);
         }
         else if (randomizedQuestionSet.length < currentQuestionIndex + 1) {
+            wrongFeedback();
             console.log("wrong"); 
             selectedAnswer.style.backgroundColor="red";
             feedbackEl.textContent = "";
@@ -156,6 +148,7 @@ var selectAnswer = function(event) {
             setTimeout(endQuiz, 500);
         }
         else if (selectedAnswer.value == questionObj.correct) {
+            correctFeedback();
             console.log("correct"); 
             selectedAnswer.style.backgroundColor="green";
             feedbackEl.textContent = "";
@@ -166,6 +159,7 @@ var selectAnswer = function(event) {
             setTimeout(nextQuestion, 500);
         }
         else {
+            wrongFeedback();
             console.log("wrong"); 
             selectedAnswer.style.backgroundColor="red";
             feedbackEl.textContent = "";
@@ -189,21 +183,11 @@ var viewHighScores = function(event) {
     };
 };
 
-//adapted from stackoverflow timer code
-var startCountdown = function(duration, display) {
-    var countdown = duration, minutes, seconds;
-    setInterval(function(){
-        minutes = parseInt(countdown / 60, 10);
-        seconds = parseInt(countdown % 60, 10);
+//adapted from w3schools timer code
+var startCountdown = function() {}
 
-        seconds = seconds < 10 ? "0" + seconds: seconds;
-        display.textContent = seconds;
-        if (--countdown < 0) {
-            countdown = duration;
-        }
-    },1000);
-
-};
+// Set the date we're counting down to
+var countDownTime = 120000;
 
 //call startGame function on click
 startButtonEl.addEventListener("click", startGame);
